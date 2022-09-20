392666bd6371eefe4234be81ee7b101e670882a8
LeaTiedemann <lea.tiedemann@t-online.de>
2022-09-20 08:18:57 +0200

    Update conf.py

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -26,6 +26,7 @@ version = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import sphinxcontrib
 
 extensions = [
     'sphinx.ext.autodoc',
